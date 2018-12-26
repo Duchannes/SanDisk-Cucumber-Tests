3f728b098d8fd39407ec53928a44f3f72fddc2ed
Dzmitry_Aldoshin <Dzmitry_Aldoshin@epam.com>
2018-12-26 19:22:49 +0300

    config rework with eslint rules

--- test/SanDisk/config/config.js
+++ test/SanDisk/config/config.js
@@ -1,27 +1,28 @@
-const path = require('path')
-const yargs = require('yargs')
+/* eslint-disable no-undef */
+const path = require(`path`);
+const yargs = require(`yargs`);
 
 exports.config = {
-    allScriptsTimeout: 20000,
-    getPageTimeout: 20000,
-    specs: [path.resolve('./test/SanDisk/features/*.feature')],
-    framework: 'custom',
-    frameworkPath : require.resolve('protractor-cucumber-framework'),
+  allScriptsTimeout: 20000,
+  getPageTimeout: 20000,
+  specs: [path.resolve(`./test/SanDisk/features/*.feature`)],
+  framework: `custom`,
+  frameworkPath: require.resolve(`protractor-cucumber-framework`),
+  ignoreUncaughtExceptions: true,
+  capabilities: {
+    browserName: yargs.browser || `chrome`,
+    shardTestFiles: yargs.instances > 1,
+    maxInstances: yargs.instances || 1
+  },
+  disableChecks: true,
+  cucumberOpts: {
+    require: [path.resolve(`./test/SanDisk/steps/**/*.js`)],
     ignoreUncaughtExceptions: true,
-    capabilities: {
-        browserName: yargs.browser || 'chrome',
-        shardTestFiles: yargs.instances > 1,
-        maxInstances: yargs.instances || 1
-    },
-    disableChecks: true,
-    cucumberOpts: {
-        require: [path.resolve('./test/SanDisk/steps/**/*.js')],
-        ignoreUncaughtExceptions: true,
-        format: 'json:./reports/report.json',
-        tags: yargs.tag || '@sandisk'
-    },
-    onPrepare: () => {
-        logger.info('Maximizing browser window');
-        browser.manage().window().maximize();
-    }
-}
+    format: `json:./reports/report.json`,
+    tags: yargs.tag || `@sandisk`
+  },
+  onPrepare: () => {
+    logger.info(`Maximizing browser window`);
+    browser.manage().window().maximize();
+  }
+};
