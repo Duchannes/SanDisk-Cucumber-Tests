8cb29e8beaeae461802f02ae5a885f3bfbb3f473
Dzmitry_Aldoshin <Dzmitry_Aldoshin@epam.com>
2019-01-02 16:49:09 +0300

    c1

--- test/SanDisk/steps/stepFunctions.js
+++ test/SanDisk/steps/stepFunctions.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 const path = require(`path`);
 const pageSelector = require(path.resolve(`./test/SanDisk/utils/pageSelector.js`));
+const logger = require(path.resolve(`./test/SanDisk/config/loggerConfig.js`)).logger;
 
 let getPageObjectElement = async (alias) => {
   let pageElement = (await pageSelector.getPage())[alias];
@@ -13,6 +14,14 @@ let getPageObjectElement = async (alias) => {
   }
 };
 
+let getNestedElement = async (parentAlias, arrayOfAliases) => {
+  let parent = (await pageSelector.getPage())[parentAlias];
+  let children = parent.children;
+  logger.debug(JSON.stringify(parent));
+  logger.debug(JSON.stringify(children));
+  parentElement = element.all(by.css(parent.selector));
+};
 module.exports = {
-  getPageObjectElement
+  getPageObjectElement,
+  getNestedElement
 };
--- test/SanDisk/steps/validationSteps.js
+++ test/SanDisk/steps/validationSteps.js
@@ -4,6 +4,7 @@ let { Then, When, Given } = require(`cucumber`);
 const expect = require(`chai`).expect;
 const path = require(`path`);
 const elementHelper = require(path.resolve(`./test/SanDisk/steps/stepFunctions.js`)).getPageObjectElement;
+const nestedElementHelper = require(path.resolve(`./test/SanDisk/steps/stepFunctions.js`)).getNestedElement;
 const logger = require(path.resolve(`./test/SanDisk/config/loggerConfig.js`)).logger;
 
 Then(/^Text of "([^"]*)" should( not)? contain "([^"]*)"$/, async (alias, notArg, textToContain) => {
@@ -16,5 +17,10 @@ Then(/^Text of "([^"]*)" should( not)? contain "([^"]*)"$/, async (alias, notArg
 
 When(/^I click "([^"]*)"$/, async (alias) => {
   logger.info(`I click ${alias}`);
+  if (alias.includes(`>`)) {
+    let arrayOfAliases = alias.trim(` > `);
+    let parentAlias = arrayOfAliases[0];
+    return (await nestedElementHelper(parentAlias, arrayOfAliases)).click();
+  }
   return (await elementHelper(alias)).click();
 });
